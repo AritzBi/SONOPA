de2692d8f4fb502513fa7610acbc4ffba70d85c8
aritzbilbao <aritzbilbao@deusto.es>
2015-03-12 11:07:28 +0100

    Script to insert to the db the csv almost finished

--- SONOPAController/csv_to_db.py
+++ SONOPAController/csv_to_db.py
@@ -1,15 +1,12 @@
 __author__ = 'AritzBi'
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, Date, Boolean, ForeignKey,DateTime, Text
-from sqlalchemy.ext.declarative import declarative_base
 import csv
 from datetime import datetime, timedelta
 import calendar
 from calendar import timegm
 import ConfigParser
 import json
-from config import SQLALCHEMY_DATABASE_URI
-from sqlalchemy.orm import sessionmaker,relationship
+from config import DB, DB_USER,DB_PASS
+import MySQLdb
 config = ConfigParser.ConfigParser()
 config.read('config.cfg')
 period=config.getfloat('Rules','period')
@@ -17,37 +14,11 @@ json_period=config.getfloat('Rules','json_period')
 json_data=open("houseConfiguration.json")
 houseConfiguration=json.load(json_data)
 json_data.close()
-Base = declarative_base()
+lastID="SELECT LAST_INSERT_ID();"
 
-class Location(Base):
-    __tablename__ = 'Location'
-    """Represents the location of a Sensor in the system"""
-    id = Column(Integer, primary_key=True)
-    name = Column(String(64))
-    sensors = relationship('Sensor', backref='located_in', lazy='dynamic')
+conn =  MySQLdb.connect(host="localhost", user=DB_USER, passwd=DB_PASS,db=DB)
 
-
-class Sensor(Base):
-    __tablename__ = 'Sensor'
-    """Represents a sensor in the system"""
-    id = Column(Integer, primary_key=True)
-    name = Column(String(64))
-    sensorRef = Column(String(64))
-    type = Column(String(64))
-    last_alive = Column(DateTime, default=datetime.now())
-    location = Column(Integer, ForeignKey('location.id'))
-    events = relationship('Event', backref='fired_by', lazy='dynamic')
-
-
-class Event(Base):
-    __tablename__ = 'Event'
-    """Represents a sensor fired event"""
-    id = Column(Integer, primary_key=True)
-    timestamp = Column(DateTime)
-    sensor = Column(Integer, ForeignKey('sensor.id'))
-    #sensorRef = db.Column(db.Integer, db.ForeignKey('sensor.sensorRef'))
-    value = Column(Text)
-
+cursor = conn.cursor()
 with open('SensorDataSurrey.csv','rb') as csvfile:
 	formatedData=[]
 	i=0;
@@ -72,8 +43,22 @@ with open('SensorDataSurrey.csv','rb') as csvfile:
 		dataArray.append(location)
 		formatedData.append(dataArray)
 		numberOfActivations=numberOfActivations +1
-	engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
-	Session = sessionmaker()
-	Session.configure(bind=engine)
-	session = Session()
-	print formatedData
+	for data in formatedData:
+		SQLSelect="SELECT id FROM location WHERE NAME=%s;"
+		cursor.execute(SQLSelect,(data[1],))
+		if cursor.rowcount==0:
+			SQLInsert="INSERT INTO location (name) VALUES (%s);"
+			cursor.execute(SQLInsert, (data[1]))
+			cursor.execute(lastID)	
+			location_id=cursor.fetchone()[0]
+			SQLInsert="INSERT INTO sensor (location) VALUES (%s);"
+			cursor.execute(SQLInsert, (location_id))
+			sensor_id=cursor.fetchone()[0]
+			
+		else:
+			SQLSelect="SELECT s.id FROM sensor s, location l WHERE l.name=%s, l.id=s.location;"
+			sensor_id=cursor.fetchone()[0]
+
+
+
+conn.commit()
